9893b49ee9b97ee1240622f4789d8ab7a8a48c78
Fabricio Bortoluzzi <fb@univali.br>
2024-03-09 17:20:59 +0100

    Minor fix

--- other/generate_capinfos.sh
+++ other/generate_capinfos.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+mkdir -p capinfos
+
 echo "capinfos start"
 for f in ../*.pcap.gz
 do
 	echo "generating capinfos from pcaps in parent directory"
-	capinfos ${f} > '${f}.capinfos.txt'
+	capinfos ${f} > 'capinfos/${f}.capinfos.txt'
 done
 echo "capinfos end"
