1fdaf43d3b1178d51a7ef528425f5936bd722c13
Fabricio Bortoluzzi <fb@univali.br>
2024-03-09 18:46:24 +0100

    Update 1_generate_tshark_csv.sh
    Minor fix

--- 1_generate_tshark_csv.sh
+++ 1_generate_tshark_csv.sh
@@ -4,7 +4,6 @@ if [ -f nohup.out ]; then
 	rm nohup.out
 fi
 
-
 if [ -f output.csv.gz ]; then
 	mv output.csv.gz output.csv.gz.old
 fi
@@ -17,10 +16,8 @@ echo "tshark start"
 for f in *.pcap.gz
 do
 	echo "extracting data from ${f}"
-	time tshark -r $f -M 100000 -t ad -T fields -e _ws.col.Time -e ip.src -e ip.dst -e tcp.dstport -e udp.dstport -e icmp.type -Eseparator=, --log-level "critical" >> output.csv
+	# -O ip,tcp,udp,icmp # Barry's advice
+	# --log-level "critical" # this works in Ubuntu but not in Rocky
+	time tshark -r $f -M 100000 -t ad -T fields -e _ws.col.Time -e ip.src -e ip.dst -e tcp.dstport -e udp.dstport -e icmp.type -Eseparator=, | gzip -c >> output.csv.gz
 done
 echo "tshark end"
-
-echo "gzipping output.csv"
-gzip output.csv
-echo "gzip end"
