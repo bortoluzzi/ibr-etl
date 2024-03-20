9366a4ca8786a1ec62e1984ed7aa89050ec5e100
Fabricio Bortoluzzi <fb@univali.br>
2024-03-21 00:00:42 +0100

    Update full_etl.sh

--- other/full_etl.sh
+++ other/full_etl.sh
@@ -8,4 +8,4 @@ step1=' ${prefix}1_generate_tshark_csv.sh &&'
 step2=' zcat output.csv.gz | python3 ${prefix}2_enrich_csv.py | gzip > output.rich.csv.gz &&'
 step3=' zcat output.rich.csv.gz | python3 ${prefix}3_summarize_radiation.py > output.tex '
 
-eval ($step0 + $step1 + $step2 + $step3)
+echo $step0 + $step1 + $step2 + $step3
