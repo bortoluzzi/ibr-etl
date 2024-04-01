02b449604be7de5afba847a3e1560efa19293c93
Fabricio Bortoluzzi <fb@univali.br>
2024-04-01 18:42:48 +0200

    Update import_pcaps_onto_securityonion.sh

--- other/import_pcaps_onto_securityonion.sh
+++ other/import_pcaps_onto_securityonion.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo "Run this as root due to so-import-pcap and sudo timeouts"
+
 for file in *.pcap.gz
 do
     basename="${file%%.*}"
     echo "Decompressing ${file} into ${basename}.pcap"
     zcat ${file} > ${basename}.pcap
     echo "Importing ${basename}.pcap"
-    sudo so-import-pcap ${basename}.pcap > ${basename}.so.pcap.txt
+    so-import-pcap ${basename}.pcap > ${basename}.so.pcap.txt
     rm ${basename}.pcap
 done
